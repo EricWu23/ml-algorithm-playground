3ac3875843a307bdff6c1043e6741df30bbff19f
Yujiang Wu <wuyujiangacademy@yahoo.com>
2026-01-19 22:09:33 -0500

    feat(logistic regression): v2 - use (d,) instead (d,1) to simplify shaping management

--- src/ml_algorithm_playground/linear/logistic_regression.py
+++ src/ml_algorithm_playground/linear/logistic_regression.py
@@ -1,7 +1,7 @@
 import numpy as np
 class LogisticRegression:
-  def __init__(self, treshold=0.5,lr=0.01, max_iter=1000, tol=1e-4):
-    self.treshold = treshold
+  def __init__(self, threshold=0.5,lr=0.01, max_iter=1000, tol=1e-4):
+    self.threshold = threshold
     self.lr = lr
     self.max_iter = max_iter
     self.tol = tol
@@ -10,6 +10,7 @@ class LogisticRegression:
 
 
   def sigmoid(self, z):
+    z=np.clip(z, -500, 500)
     return 1/(1+np.exp(-z))
   
   def initialize_weights(self, n_features):
@@ -23,27 +24,27 @@ class LogisticRegression:
     n_samples, n_features = X.shape
     self.initialize_weights(n_features)
     for i in range(self.max_iter):
-      z = (X@self.weights[:,None] + self.bias)#（n,d）@(d,1) = (n,1 )
-      assert z.shape == (n_samples,1)
+      z = (X@self.weights + self.bias)#（n,d）@(d,) = (n,)
+      assert z.shape == (n_samples,)
       y_hat = self.sigmoid(z)
-      assert y_hat.shape == (n_samples,1)
-      err = y_hat - y[:,None]
-      assert err.shape == (n_samples,1)
-      dw =1/n_samples*X.T@err#(d,n)@(n,1) = (d,1)
+      assert y_hat.shape == (n_samples,)
+      err = y_hat - y
+      assert err.shape == (n_samples,)
+      dw =1/n_samples*X.T@err#(d,n)@(n,) = (d,)
       db = 1/n_samples*np.sum(err)
-      assert dw.shape == (n_features,1)
-      self.weights -=self.lr*(dw.reshape(-1,))
+      assert dw.shape == (n_features,)
+      self.weights -=self.lr*dw
       self.bias -=self.lr*db
       if np.linalg.norm(dw)<self.tol:
         break
     return self
   
   def predict_proba(self,X):
-    z = (X@self.weights[:,None] + self.bias)
+    z = (X@self.weights + self.bias)
     y_hat = self.sigmoid(z)
     return y_hat
   
   def predict(self,X):
     y_hat = self.predict_proba(X)
-    return (y_hat>self.treshold).astype(int)
+    return (y_hat>=self.threshold).astype(int)
   
