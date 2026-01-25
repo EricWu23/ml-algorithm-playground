33e82e3cc05ea7eda92a14f861062bc8f0609559
Yujiang Wu <wuyujiangacademy@yahoo.com>
2026-01-24 23:58:32 -0500

    chore (linear regression): input shape contract y = y.reshape(-1, 1)

--- src/ml_algorithm_playground/linear/linear_regression.py
+++ src/ml_algorithm_playground/linear/linear_regression.py
@@ -3,36 +3,36 @@ class LinearRegression:
     def __init__(self,lr=0.01,max_iter=1000,tol=1e-6):
         self.lr = lr
         self.max_iter = max_iter
-        self.W = None
+        self.w = None
         self.b = None
-    
-    # utility functions
-    def identity(self,X):
-        return X
     
     # initialization
     def __initialization(self,X,y):
         n, d = X.shape
         _, q = y.shape
-        self.W = np.zeros((d,q))
+        self.w = np.zeros((d,q))
         self.b = np.zeros(q)
 
     def fit(self,X,y):
+        assert X.ndim ==2
+        assert y.ndim in [1,2]
+        if y.ndim == 1:
+            y = y.reshape(-1, 1)
         n, d = X.shape
-        _, q = y.shape
+        q = y.shape[1]
         self.__initialization(X,y)
 
         for i in range(self.max_iter):
-            z= X@self.W + self.b
-            y_pred = self.identity(z)
+            z= X@self.w + self.b
+            y_pred = z
             error = y_pred - y#n,q
             dw = (1/n)* X.T@error#d,q
             db = (1/n)* np.sum(error,axis=0)#q
-            self.W = self.W - self.lr * dw
+            self.w = self.w - self.lr * dw
             self.b = self.b - self.lr * db
         return self
     
     def predict(self,X):
-        z= X@self.W + self.b
-        y_pred = self.identity(z)
+        z= X@self.w + self.b
+        y_pred = z
         return y_pred
